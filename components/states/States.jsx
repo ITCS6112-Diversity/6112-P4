3444fcfb217e3351726b3fa00b7975d797ba840d
Ryan <rbass6@uncc.edu>
2023-09-27 12:31:46 -0400

    Finish problem 2

--- components/states/States.jsx
+++ components/states/States.jsx
@@ -10,12 +10,57 @@ class States extends React.Component {
   constructor(props) {
     super(props);
     console.log('window.models.states', window.models.states);
+    this.state = {
+      states: window.models.states(),
+      updatedStates: [],
+      inputValue: ''
+    };
+
+    this.handleChangeBound = event => this.handleChange(event);
+  }
+
+  // When text is inputted, filter state array to only include matching substring
+  handleChange(event) {
+    this.setState({ inputValue : event.target.value });
+    let results = this.state.states.filter((str) => str.toLowerCase().includes(event.target.value.toLowerCase()));
+    this.setState({ updatedStates: results });
   }
 
   render() {
+
+    // If there's an input, display filtered states. Otherwise, display all.
+    let stateResults;
+    if (this.state.inputValue.length !== 0){
+      stateResults = (
+        this.state.updatedStates.map((s) => (
+          <li key={s}>{s}</li>
+        ))
+      );
+    } else {
+      stateResults = (
+        this.state.states.map((s) => (
+          <li key={s}>{s}</li>
+        ))
+      );
+    }
+
     return (
-      <div>
-        Replace this with the code for Project 4, Problem 2
+      <div className="states-container">
+        <div className="states-input-container">
+          <form>
+            <label htmlFor="stateInput">Search for states:</label>
+            <input type="text" id="stateInput" placeholder="Enter state here" onChange={(e) => {this.handleChangeBound(e);}}/>
+          </form>
+        </div>
+        <div className="states-display-container">
+          <h1>States</h1>
+          <hr/>
+          <div className="states-list-container">
+            <ol>
+              {stateResults}
+            </ol>
+          </div>
+        </div>
       </div>
     );
   }
